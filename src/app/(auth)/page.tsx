839c1100551e3fc33045f83c4621e795d7897e21
Edvan Lima <edvan.lima@tripletech.com.br>
2023-12-15 10:18:30 -0300

    resolvendo error Permissions-Policy header

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,12 +1,21 @@
+import { headers } from "next/headers"
+import Script from "next/script"
 import { Shell } from "@/components/shells/shell"
 import { AuthForm } from "@/components/forms/auth-form"
 import { Toaster } from "sonner"
 
 const AuthPage = () => {
+  const nonce = headers().get("x-nonce")
+  if (!nonce) return
   return (
     <Shell className="max-w-lg">
       <AuthForm />
       <Toaster />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js"
+        strategy="afterInteractive"
+        nonce={nonce}
+      />
     </Shell>
   )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,31 @@ import { getToken } from "next-auth/jwt"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function middleware(req: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64")
+  const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+  style-src 'self' 'nonce-${nonce}';
+  img-src 'self' blob: data:;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  block-all-mixed-content;
+  upgrade-insecure-requests;
+`
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim()
+
+  const requestHeaders = new Headers(req.headers)
+  requestHeaders.set("x-nonce", nonce)
+  requestHeaders.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
+  )
+
   const pathname = req.nextUrl.pathname
   const protectedPaths = [
     "/dashboard/client",
@@ -11,7 +36,12 @@ export async function middleware(req: NextRequest) {
     "/dashboard/attendance/new",
   ]
   const isPathProtected = protectedPaths?.some((path) => pathname == path)
-  const res = NextResponse.next()
+  const res = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+  res.headers.set("Content-Security-Policy", contentSecurityPolicyHeaderValue)
   if (isPathProtected) {
     const token = await getToken({ req })
 
@@ -26,4 +56,23 @@ export async function middleware(req: NextRequest) {
     // }
   }
   return res
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
 }
